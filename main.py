1227fe8e13c8d3bbb755eb36e30bfe99ce204b6f
LucientZ <koblenzpoisiden@gmail.com>
2022-11-26 14:35:11 -0600

    [ADD/CHANGE] Added syncid implementation of token handling

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ class aclient(discord.Client):
     async def on_ready(self):
         await self.wait_until_ready()
         if not self.synced:
-            await tree.sync(guild = discord.Object(id = 873027766331797575))
+            await tree.sync(guild = discord.Object(id = get_token(".syncid", "Command Syncing")))
             self.synced = True
         print(f"Bot is ready and logged in as {self.user}.")
 
@@ -22,63 +22,64 @@ tree = app_commands.CommandTree(client)
 async def slash_ping(ctx: discord.Interaction, name: str):
     await ctx.response.send_message(f"Pong! {int(client.latency * 1000)} ms")
 
-def write_token(TOKEN) -> None:
+def write_token(TOKEN: str, filename: str) -> None:
     """
-    Writes a token to .token.
-    If .token does not exist, creates .token and writes the token.
+    Writes a token to a specified file.
+    If filename does not exist, creates filename and writes the token.
 
     Parameters:
     TOKEN (str): Token to be written to file
+    filename (str): File for TOKEN to be written
 
     Returns:
     None
     """
-    tokenfile = open(".token", "w")
-    tokenfile.write(TOKEN)
-    tokenfile.close()
+    with open(filename, "w") as tokenfile:
+        tokenfile.write(TOKEN)
+        tokenfile.close()
 
-def get_token() -> str:
+def get_token(filename: str, use_case = "Unspecified") -> str:
     """
-    Obtains token from a file named '.token' or from the user.
-    If '.token' does not have a token or is empty, asks the user if they would like to write to '.token'
+    Obtains token from a file specified asks the user.
+    If the file does not have a token or is empty, asks the user if they would like to write to the file
 
     Parameters:
-    None
+    filename (str): name of file to be used in obtaining token
 
     Returns:
     str: token to be used by bot
     """
-    # Initializes TOKEN and choice as strings
+    # Initializes TOKEN and choice as empty strings
     TOKEN = ""
     choice = ""
 
-    #At the moment, this code assumes the only error is .token not existing
+    #Currently, this code assumes the only error is the file not existing
     try:
-        # opens up .token which should only contain the bot token
-        with open('.token') as tokenfile:
-            print("Logging in with TOKEN from .token")
+        # opens up filename which should only contain the bot token
+        with open(filename) as tokenfile:
+            print(f"Using token from '{filename}' for use case: {use_case}")
             TOKEN = tokenfile.read()
 
             if(TOKEN == ""):
-                # If TOKEN == "", this means that .token is empty
+                # If TOKEN == "", this means that the file is empty
                 
-                while(choice != "Y" and choice != "n"):
-                    choice = input("\nIt looks like there isn't anything in '.token'.\nWould you like to add a token to this file? (It is recommended that this is done manually) [Y/n] ")
+                while(choice.lower() != "y" and choice.lower() != "n"):
+                    choice = input(f"\nIt looks like there isn't anything in '{filename}'.\nWould you like to add a token to this file? [Y/n] ")
                 
-                if(choice == 'Y'):
+                if(choice.lower() == 'y'):
                     TOKEN = input('\nPlease enter the bot token: ')
-                    write_token(TOKEN)
+                    write_token(TOKEN, filename)
                 else:
-                    print(f"\nNo token will be added to '.token'")
+                    print(f"\nNo token will be added to '{filename}'")
                     TOKEN = input('Please enter the bot token: ')                
     except OSError:
-        # Handling when .token does not exist.
+        # Handling when the file does not exist.
         while(choice.lower() != "y" and choice.lower() != "n"):
-            choice = input("\nIt looks like there isn't a file named '.token' in this directory.\nWould you like to create this file? [Y/n] ")
+            choice = input(f"\nIt looks like there isn't a file named '{filename}' in this directory.\nWould you like to create this file? [Y/n] ")
 
         if(choice.lower() == 'y'):
             TOKEN = input('\nPlease enter the bot token: ')
-            write_token(TOKEN)
+            write_token(TOKEN, filename)
         else:
             print("\n.dat will not be created")
             TOKEN = input('Please enter the bot token: ')
@@ -90,7 +91,7 @@ def main():
 
     # Attempt to login with token
     try:
-        TOKEN = get_token()
+        TOKEN = get_token(".token", "Logging In")
         client.run(TOKEN)
     except discord.LoginFailure as e:
         #This error is raised when the token is not valid
@@ -101,7 +102,7 @@ def main():
             choice = input("\nWould you like to write a new token? [Y/n] ")    
         if(choice.lower() == "y"):
             TOKEN = input("Please enter bot token: ")
-            write_token(TOKEN)
+            write_token(TOKEN, ".token")
         print("Terminating program...\n")
         exit()
     except Exception as e:
